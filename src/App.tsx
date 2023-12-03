ce044f958ea037ea9130b4dcb6312df286bac501
simon0820s <simon0820s@gmail.com>
2023-12-03 12:43:39 -0500

    initial animation

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Nav from './components/Nav'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Nav />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css'
+
+const Nav = () => {
+
+  return (
+    <div className='nav-container'>
+      <h2 className='nav-title'>Sar</h2>
+    </div>
+  )
+}
+
+export default Nav
