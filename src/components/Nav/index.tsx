9d6f8b7cad52decdbfbf70bf2a6b4293aacc3757
simon0820s <simon0820s@gmail.com>
2023-12-03 23:46:53 -0500

    initial animation ready

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,12 +1,14 @@
-import './styles.css'
+import "./styles.css";
 
 const Nav = () => {
-
   return (
-    <div className='nav-container'>
-      <h2 className='nav-title'>Sar</h2>
+    <div className="nav-container">
+      <video className="nav-video" autoPlay loop muted>
+        <source src="public/nav-background-video.mp4" type="video/mp4" />
+      </video>
+      <h2 className="nav-title">Sar</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
