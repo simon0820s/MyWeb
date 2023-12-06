246e3afd468ca16bc63c797f974b690413d34383
simon0820s <simon0820s@gmail.com>
2023-12-06 13:02:59 -0500

    use intersection observer

--- src/components/AboutMeTitle/index.tsx
+++ src/components/AboutMeTitle/index.tsx
@@ -1,9 +1,43 @@
+import { useEffect, useRef, useState } from "react";
 import "./styles.css";
 
 const Title = () => {
+  const textContainerRef = useRef<HTMLDivElement>(null);
+  const [isAnimated, SetIsAnimated] = useState(false);
+
+  useEffect(() => {
+    const textContainer = textContainerRef.current;
+
+    if (!textContainer) return;
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            SetIsAnimated(true);
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
+
+    observer.observe(textContainer);
+    
+    return () => {
+      if (textContainer) {
+        observer.unobserve(textContainer)
+      }
+    }
+  }, []);
+
+  const textContainerClass = isAnimated
+    ? "about-text-container animated"
+    : "about-text-container";
+
   return (
-    <div className="about-title-container">
-      <div className="about-text-container">
+    <div ref={textContainerRef} className="about-title-container">
+      <div className={textContainerClass}>
         <h2 className="special-text">Machine learning Engineer</h2>
         <h2 className="special-text">Rust Developer</h2>
         <h2 className="special-text">Web-fullstack Engineer</h2>
