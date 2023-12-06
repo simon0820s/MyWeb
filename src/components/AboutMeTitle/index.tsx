5f13419b09aa1da73283b8bfea136754517470f3
simon0820s <simon0820s@gmail.com>
2023-12-06 12:21:47 -0500

    animate about section

--- src/components/AboutMeTitle/index.tsx
+++ src/components/AboutMeTitle/index.tsx
@@ -5,8 +5,8 @@ const Title = () => {
     <div className="about-title-container">
       <div className="about-text-container">
         <h2 className="special-text">Machine learning Engineer</h2>
-        <h2 className="special-text">Web-fullstack Engineer</h2>
         <h2 className="special-text">Rust Developer</h2>
+        <h2 className="special-text">Web-fullstack Engineer</h2>
         <h2 className="special-text">Computer Science</h2>
         <h2 className="special-text">I'm</h2>
       </div>
