b79781d075c89117b9558cb83a999280a52c7fe2
simon0820s <simon0820s@gmail.com>
2023-12-07 13:08:15 -0500

    card contact me

--- src/components/ContactMe/index.tsx
+++ src/components/ContactMe/index.tsx
@@ -1,11 +1,20 @@
-import './styles.css'
+import "./styles.css";
 
 const ContactMe = () => {
   return (
-    <div className="contactMe-container">
-      <h2 className='special-text'>Contact Me</h2>
+    <div className="contactMe-box">
+      <span></span>
+      <div className="card-content">
+        <span></span>
+        <h2>Contact Me</h2>
+        <p className="normal-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
+          molliti
+        </p>
+        <a href="">Read More</a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ContactMe
+export default ContactMe;
