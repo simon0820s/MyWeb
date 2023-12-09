17a72c10b8881420a8b365e6038fff5ea0e7cc7e
simon0820s <simon0820s@gmail.com>
2023-12-09 16:22:22 -0500

    create code button

--- src/sections/AboutMyCode/index.tsx
+++ src/sections/AboutMyCode/index.tsx
@@ -9,9 +9,13 @@ const AboutMyCode = () => {
           <h2 className="title special-text">About my code</h2>
           <span />
           <p className="normal-text code-article-text">
-            I have experience in programming, artificial intelligence, front-end and back-end. I strive to write clear and easy-to-maintain code, always
-            aiming for excellence and adaptability in my projects.
+            I have experience in programming, artificial intelligence, front-end
+            and back-end. I strive to write clear and easy-to-maintain code,
+            always aiming for excellence and adaptability in my projects.
           </p>
+          <a href="https://github.com/simon0820s" className="git-button" target="_blank">
+            Look at my code
+          </a>
         </article>
         <img className="my-photo" src="./img/myPhoto.jpeg" alt="my-photo" />
       </Layout>
