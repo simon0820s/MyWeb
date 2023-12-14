1069ed56763002375b68a6f8065e9b64f14c8962
simon0820s <simon0820s@gmail.com>
2023-12-14 14:06:38 -0500

    1024px responsive ready

--- src/sections/AboutMe/index.tsx
+++ src/sections/AboutMe/index.tsx
@@ -1,7 +1,7 @@
 import AboutMeArticle from "../../components/AboutMeAricle";
 import HorizontalSeparator from "../../components/HorizontalSeparator";
 import Layout from "../../components/Layout";
-import AboutMeTitle from '../../components/AboutMeTitle'
+import AboutMeTitle from "../../components/AboutMeTitle";
 import ContactMe from "../../components/ContactMe";
 import "./styles.css";
 
@@ -9,12 +9,17 @@ const AboutMe = () => {
   return (
     <section id="about-me">
       <Layout>
-        <div className="about-me-article">
-          <AboutMeTitle />
-          <HorizontalSeparator />
-          <AboutMeArticle />
+        <div className="about-me-container">
+          <div className="about-me-left">
+            <div className="about-me-article">
+              <AboutMeTitle />
+              <HorizontalSeparator />
+              <AboutMeArticle />
+            </div>
+            <ContactMe />
+          </div>
+          <div className="about-me-rigth">Im the rigth Section</div>
         </div>
-        <ContactMe />
       </Layout>
     </section>
   );
